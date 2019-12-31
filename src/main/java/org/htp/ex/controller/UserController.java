3210c4a3f348d697424be30f2beb9d76bc2fd335
Vladislav Bedritsky <vladquinn2010@gmail.com>
2019-12-31 14:14:23 +0300

    1.03

--- src/main/java/org/htp/ex/controller/UserController.java
+++ src/main/java/org/htp/ex/controller/UserController.java
@@ -1,6 +1,5 @@
 package org.htp.ex.controller;
 
-import org.htp.ex.model.Ticket;
 import org.htp.ex.model.Trip;
 import org.htp.ex.model.User;
 import org.htp.ex.service.MainService;
@@ -10,13 +9,10 @@ import org.htp.ex.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
 
